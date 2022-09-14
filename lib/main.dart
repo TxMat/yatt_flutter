479c6a659e02f8e1cf4b9284b997f830592edd87
TxMat <mathieu.ponal@gmail.com>
2022-09-14 12:48:14 -0400

    themes + test app

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,17 +12,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: const MaterialColor(0xFF6cb44b, <int, Color>{
+            50: Color(0xFFEDF6E9),
+            100: Color(0xFFD3E9C9),
+            200: Color(0xFFB6DAA5),
+            300: Color(0xFF98CB81),
+            400: Color(0xFF82BF66),
+            500: Color(0xFF6CB44B),
+            600: Color(0xFF64AD44),
+            700: Color(0xFF59A43B),
+            800: Color(0xFF4F9C33),
+            900: Color(0xFF3D8C23),
+          }),
+          scaffoldBackgroundColor: const Color(0xFF171717),
+          textTheme: const TextTheme(
+                  bodyText1: TextStyle(),
+                  bodyText2: TextStyle(),
+                  headline4: TextStyle())
+              .apply(
+            bodyColor: const Color(0xFFF9F9F9),
+            displayColor: const Color(0xFFF9F9F9),
+          )),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -96,6 +114,9 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            const BackButton(
+              color: Colors.lightGreen,
             ),
             FlutterLogo(
               size: _counter.toDouble(),
